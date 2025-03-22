4296afaa55ba727032c2b300e7221a393b425b4e
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-03-22 13:13:53 -0400

    Project Init

---
+++ NanoDNA.DockerManager/DockerContainer.cs
@@ -0,0 +1,39 @@
+﻿
+namespace NanoDNA.DockerManager
+{
+    internal class DockerContainer
+    {
+
+        /// <summary>
+        /// Name of the Docker Container
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ID of the Docker Container
+        /// </summary>
+        public long ID { get; set; }
+
+        /// <summary>
+        /// Image of the Docker Container
+        /// </summary>
+        public string Image { get; set; }
+
+
+        public DockerContainer ()
+        {
+
+        }
+
+        public void Execute ()
+        {
+            //Executes Docker Command
+        }
+
+
+
+
+
+
+    }
+}
